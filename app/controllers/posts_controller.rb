f634ec5b1f5f158294272417433fdafa92f9a7d1
Joseph Bourne <kiratsuchi@live.com>
2017-05-01 20:54:43 -0700

    fixed the post controller and view error. almost done. just need to add couple more methods to the post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,26 @@
 class PostsController < ApplicationController
-  def new
 
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to root_url, notice: 'Post created'
+    else
+      redirect_to new_post_path, notice: 'Post creation failed. Try again!'
+    end
+  end
+
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body, :user_id)
   end
 end
